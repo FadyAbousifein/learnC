19570e13c2ff1a4f40102af73d6fc14a641ab248
Fady Abousifein <fadyasifein@gmail.com>
2025-12-11 14:55:37 -0500

    test push

--- test.c
+++ test.c
@@ -1,7 +1,9 @@
 #include <stdio.h> 
 
 int main(int argc, char *argv[]) {
-    int x = 8; 
-    printf("The value of your variable is %e", x); 
+    int x[9] = {1,4,5,6};
+    for (int i = 0; i < 9; i++) {
+        printf("%d ", x[i]);  
+    }
     return 0;
 }
